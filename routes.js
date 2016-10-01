4d2af76f86913ae82cca849e5beb220cb37b1db0
Carlos Fernández <sirikon2@gmail.com>
2016-10-01 17:27:46 +0200

    Add root route

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 var picturesController = require('./controllers/pictures-controller');
 
 module.exports = (app) => {
+    app.get('/', (req, res) => { res.send("This is GeoPic"); });
     app.post.apply(app, ['/api/pictures'].concat(picturesController.addPicture));
     app.get.apply(app, ['/api/pictures'].concat(picturesController.getPictures));
     app.get.apply(app, ['/api/pictures/:pictureId.jpg'].concat(picturesController.getPictureResource));
