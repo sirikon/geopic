e841685ccd42f456f957a26b48574ae7b91656c1
Carlos Fernández <sirikon2@gmail.com>
2016-10-01 19:32:25 +0200

    little change

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@
 var picturesController = require('./controllers/pictures-controller');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => { res.send("This is GeoPic"); });
+    app.get('/', (req, res) => { res.send("This is GeoPic Deployed with Molly"); });
     app.post.apply(app, ['/api/pictures'].concat(picturesController.addPicture));
     app.get.apply(app, ['/api/pictures'].concat(picturesController.getPictures));
     app.get.apply(app, ['/api/pictures/:pictureId.jpg'].concat(picturesController.getPictureResource));
